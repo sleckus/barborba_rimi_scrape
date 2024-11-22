253b9cc481aef4703088420b0659317e7c16ef74
sleckus <lukassleckus@gmail.com>
2024-11-22 13:36:53 +0200

    maxima jau nuscrapinta ir sutalpinta i DB.

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import time
-
+from datetime import datetime
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import mysql.connector
-
-conn = mysql.connector.connect(
-    host="127.0.0.1",  # local
-    user='root',
-
-    password='',
-    database="supermarkets")
+from models.db import get_db_connection, clear_table, insert_milk_data
 
 def disCookies(driver):
     try:
@@ -23,8 +16,7 @@ def disCookies(driver):
         cookie_accept_button.click()
         print("Cookie prompt dismissed.")
     except Exception as e:
-        print(f"Cookie prompt not found or could not be dismissed: {e}")
-
+        print(f"Cookie prompt not found: {e}")
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -34,23 +26,60 @@ driver.get("https://www.barbora.lt/pieno-gaminiai-ir-kiausiniai/pienas/pasterizu
 disCookies(driver)
 
 link_list = []
-time.sleep(2)
+time.sleep(1)
 container = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[3]/div/div[3]/div[2]/div/ul')
 list_items = container.find_elements(By.TAG_NAME, 'li')
 for item in list_items:
-    link = item.find_element(By.TAG_NAME,"a").get_attribute('href')
+    link = item.find_element(By.TAG_NAME, "a").get_attribute('href')
     print(link)
     link_list.append(link)
 
+conn = get_db_connection()
+clear_table(conn, "maxima_milk")
+
 for link in link_list:
     driver.get(link)
-    price_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//*[@id="fti-product-price--0"]/div[1]/div[1]'))
-    )
-    price_text = price_element.text.replace("\n","").strip()
-    print(f"Price: {price_text}")
+    time.sleep(0.5)
+    price_element = WebDriverWait(driver, 2).until(
+        EC.presence_of_element_located(
+            (By.XPATH, '/html/body/div[2]/div/div[3]/div/div[3]/div/div[2]/div[1]/div/div[2]/h1')))
+    title = price_element.text.strip()
+    print(title)
 
-    time.sleep(1)
+    price_element = WebDriverWait(driver, 2).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="fti-product-price--0"]/div[1]/div[1]')))
+    price_text = re.sub(r"[^0-9.,]", "", price_element.text.replace("\n", "").strip()).replace(",", ".")
+    print(f"Price: {price_text[:4]} Eur")
+
+    price_element = WebDriverWait(driver, 2).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="fti-product-price--0"]/div[1]/div[2]')))
+    price_per = re.sub(r"[^0-9.,]", "", price_element.text.replace("\n", "").strip()).replace(",", ".")
+    if len(price_text) > 4:
+        price_per = price_text[4:]
+    print(f"Price per liter: {price_per} Eur/L")
+
+    try:
+        fat_element = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '/html/body/div[2]/div/div[3]/div/div[3]/div/div[3]/div/table/tbody/tr[2]/td[2]')))
+        fat_per = re.sub(r"[^0-9.,]", "", fat_element.text.replace("\n", "").strip()).replace(",", ".")
+        print(f"Fat percentage: {fat_per} g")
+    except Exception as e:
+        print(f"Failed to get fat percentage from the page: {e}")
+
+    volume = float(price_text[:4]) / float(price_per)
+    package_size = round(round(volume, 3) * 1000, -2)
+    print(f"Package size is: {package_size} milliliters")
+
+    scraped_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    insert_milk_data(conn, title, fat_per, package_size, price_text, price_per, scraped_at)
+    package_size = 0
+    fat_per = 0
+    price_per = 0
+    price_text = 0
+    print("=" * 30)
 
 
-
+driver.quit()
+conn.close()
--- models/db.py
+++ models/db.py
@@ -1,16 +1,28 @@
 import mysql.connector
 
+def get_db_connection():
+    conn = mysql.connector.connect(
+        host="127.0.0.1",  # local
+        user='root',
+        password='',
+        database="supermarkets"
+    )
+    return conn
 
-class DB():
-    def __init__(self):
-        self.connectDB()
-    def connectDB(self):
-           self.conn = mysql.connector.connect(
-               host="127.0.0.1",  # localhost
-               user='root',
-               password='',  # rašot savo
-               database="supermarkets"  # rašot savo
-           )
+def clear_table(conn, table_name):
+    cursor = conn.cursor()
+    cursor.execute(f"DELETE FROM {table_name}")
+    cursor.execute(f"ALTER TABLE {table_name} AUTO_INCREMENT = 1")  # Reset auto-increment to 1
+    conn.commit()
+    cursor.close()
+    print(f"Table {table_name} cleared and auto-increment reset.")
 
-    def close(self):
-        self.conn.close()
+def insert_milk_data(conn, name, fat_content, package_size, price, price_per_kg, scraped_at):
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO maxima_milk (name, fat_content, package_size, price, price_per_kg, scraped_at) "
+        "VALUES (%s, %s, %s, %s, %s, %s)",
+        (name, fat_content, package_size, price, price_per_kg, scraped_at)
+    )
+    conn.commit()
+    cursor.close()
